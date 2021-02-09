86d6f5c1f1569418f1e97f7af8a9058e618c1a21
Kyle Grenier <kylegrenier@users.noreply.github.com>
2021-02-09 14:48:13 -0600

    ObserverPattern Project

---
+++ ObserverPattern/Assets/Week3/4. Observer Pattern - Events/KpattManager.cs
@@ -0,0 +1,19 @@
+/*****************************************************************************
+// File Name :         KpattManager.cs
+// Author :            Kyle Grenier
+// Creation Date :     #CREATIONDATE#
+//
+// Brief Description : ADD BRIEF DESCRIPTION OF THE FILE HERE
+*****************************************************************************/
+using UnityEngine;
+
+namespace ObserverPatternEvents
+{
+    public class KpattManager : MonoBehaviour
+    {
+        public void GameOver()
+        {
+            print("GAME OVER.");
+        }
+    }
+}
