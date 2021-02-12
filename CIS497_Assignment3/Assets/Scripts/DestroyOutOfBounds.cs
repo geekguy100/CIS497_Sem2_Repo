e846ce1185e9a25f4526c417176205954989fc76
Kyle Grenier <kylegrenier@users.noreply.github.com>
2021-02-12 02:47:38 -0600

    Balls despawn out of bounds, imported some particles

---
+++ CIS497_Assignment3/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,40 @@
+/*****************************************************************************
+// File Name :         DestroyOutOfBounds.cs
+// Author :            Kyle Grenier
+// Creation Date :     02/12/2021
+//
+// Brief Description : Will destroy the game object if it is outside of the given boundaries.
+*****************************************************************************/
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    [SerializeField] private Vector2 minBoundary;
+    [SerializeField] private Vector2 maxBoundary;
+
+    private void Update()
+    {
+        Vector2 pos = transform.position;
+
+        if (OutsideMin(pos) || OutsideMax(pos))
+            Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Returns true if outside of the minimum boundary area.
+    /// </summary>
+    /// <returns>True if outside of the minimum boundary area.</returns>
+    private bool OutsideMin(Vector2 pos)
+    {
+        return (pos.x < minBoundary.x || pos.y < minBoundary.y);
+    }
+
+    /// <summary>
+    /// Returns true if outside of the maximum boundary area.
+    /// </summary>
+    /// <returns>True if outside of the maximum boundary area.</returns>
+    private bool OutsideMax(Vector2 pos)
+    {
+        return (pos.x > maxBoundary.x || pos.y > maxBoundary.y);
+    }
+}
