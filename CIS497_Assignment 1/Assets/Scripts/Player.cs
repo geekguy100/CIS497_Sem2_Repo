8484a76bf7d45bea94f236cbdc38b010352f837e
Kyle Grenier <kyle.grenier11@gmail.com>
2021-05-08 23:05:09 -0500

    Trying to get assignment 1 back

--- CIS497_Assignment 1/Assets/Scripts/Player.cs
+++ CIS497_Assignment 1/Assets/Scripts/Player.cs
@@ -1,13 +1,71 @@
 /*****************************************************************************
 // File Name :         Player.cs
 // Author :            Kyle Grenier
-// Creation Date :     #CREATIONDATE#
-//
-// Brief Description : Player class that player controls.
+// Creation Date :     1/28/2020
+// CIS497 Assignment 1
+// Brief Description : Player functionality
 *****************************************************************************/
 using UnityEngine;
+using System.Collections;
 
-public class Player : Character
-{    
+public class Player : Entity, IDamageable, IMoveable, ICanAttack
+{
+    private float movementSpeed;
+    public float health { get; set; }
 
+    private IDamageable enemy;
+
+    private void Awake()
+    {
+        setName("Player");
+        movementSpeed = 7f;
+        health = 100f;
+
+        enemy = GameObject.FindGameObjectWithTag("Zombie").GetComponent<IDamageable>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Physics.Raycast(ray, out hit);
+            Vector3 pos = hit.point;
+            pos.y = transform.position.y;
+
+            StopAllCoroutines();
+            StartCoroutine(moveTowards(pos));
+
+            //If you click on a damageable, attack it.
+            if (hit.transform != null && hit.transform.GetComponent<IDamageable>() != null && Vector3.Distance(transform.position, hit.point) < 2f)
+            {
+                attack(hit.transform.GetComponent<IDamageable>(), 20f);
+            }
+        }
+       
+    }
+
+    public void attack(IDamageable target, float damageDealt)
+    {
+        Debug.Log(entityName + " attacks for " + damageDealt + " damage.");
+        target.takeDamage(damageDealt);
+    }
+
+    public IEnumerator moveTowards(Vector3 destination)
+    {
+        while (Vector3.Distance(transform.position, destination) > 1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void takeDamage(float amnt)
+    {
+        health -= amnt;
+        if (health < 0)
+            die();
+    }
 }
--- CIS497_Assignment 1/Assets/Scripts/Zombie.cs
+++ CIS497_Assignment 1/Assets/Scripts/Zombie.cs
@@ -1,13 +1,85 @@
 /*****************************************************************************
 // File Name :         Zombie.cs
 // Author :            Kyle Grenier
-// Creation Date :     #CREATIONDATE#
-//
-// Brief Description : ADD BRIEF DESCRIPTION OF THE FILE HERE
+// Creation Date :     1/28/21
+// CIS497 Assignment 1
+// Brief Description : Zombie enemy. Moves towards and damages player.
 *****************************************************************************/
 using UnityEngine;
+using System.Collections;
 
-public class Zombie : Character
+public class Zombie : Entity, IDamageable, IMoveable, ICanAttack
 {
+    private float movementSpeed;
+    public float health { get; set; }
 
+    private GameObject player;
+    private bool tracking;
+
+    private float attackSpeed = 2f;
+    private float currentAttackTime = 0f;
+
+    private void Awake()
+    {
+        setName("Zombie");
+        movementSpeed = 5f;
+        health = 100f;
+        tracking = false;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (player == null)
+            return;
+
+        //Managing delay between attacks.
+        if (currentAttackTime < attackSpeed)
+            currentAttackTime += Time.deltaTime;
+
+        //If Zombie is out of range of player, move towards it.
+        if (Vector3.Distance(transform.position, player.transform.position) > 1f && !tracking)
+        {
+            tracking = true;
+            StartCoroutine(moveTowards(player.transform.position));
+        }
+        //If we are not tracking (in range of player), attack him.
+        else if (!tracking)
+        {
+            //Attack after delay.
+            if (currentAttackTime >= attackSpeed)
+            {
+                attack(player.GetComponent<IDamageable>(), 10f);
+                currentAttackTime = 0f;
+            }
+        }
+    }
+
+
+    public void attack(IDamageable target, float damageDealt)
+    {
+        Debug.Log(entityName + " attacks for " + damageDealt + " damage.");
+        target.takeDamage(damageDealt);
+    }
+
+    public IEnumerator moveTowards(Vector3 destination)
+    {   
+        while (Vector3.Distance(transform.position, destination) > 1f)
+        {
+            destination = player.transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        //Stop tracking now that Zombie is at target position.
+        tracking = false;
+    }
+
+    public void takeDamage(float amnt)
+    {
+        health -= amnt;
+        if (health < 0)
+            die();
+    }
 }
